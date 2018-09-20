9de476100fda63dd3eef46bea25d01d55566ebc5
timuchin51 <bezrodny83@gmail.com>
2018-09-20 21:07:00 +0300

    ---

--- scrapp/python_upcoming_events.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-
-python_org_url = 'https://www.python.org/events/python-events'
-
-html = urlopen(python_org_url)
-soup = BeautifulSoup(html, features='html.parser')
-
