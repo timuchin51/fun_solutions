f46060d60033178e7fd37977bf56ead648a5edd8
timuchin51 <bezrodny83@gmail.com>
2018-09-20 20:06:41 +0300

    finish vk_api

--- api/vk_api.py
+++ api/vk_api.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 
-from settings import access_token as token
+import os
 import requests
+from sys import argv
+
+token = os.environ['SECRET_KEY']
 
 
-code = 'return API.users.get({"user_ids": API.friends.getOnline()}); '
+def create_a_query(com_line_arguments):
+    if len(com_line_arguments) == 1:
+        return 'return API.users.get({"user_ids": API.friends.getOnline()});'
+    elif len(com_line_arguments) > 2:
+        print('Only one arguments is needed')
+    elif com_line_arguments[1].isdecimal():
+        return 'return API.users.get({"user_ids": API.friends.getOnline({"user_id":%s})});' % com_line_arguments[1]
+    else:
+        print('Input right User Id')
+        return False
 
 
 def get_request_api(execute_code):
@@ -15,14 +27,26 @@ def get_request_api(execute_code):
     return json_object
 
 
+def get_errors_msg(json_object):
+    if json_object.get('execute_errors'):
+        errors_msg = json_object['execute_errors'][0]['error_msg']
+        print(errors_msg)
+
+
 def get_names(json_object):
     user_list = json_object['response']
-    names = ['Friends online: ']
+    names = []
     for item in user_list:
-        names.append(item['first_name']+item['last_name'])
-    print(names)
+        names.append(item['first_name'] + ' ' + item['last_name'])
+    if names:
+        return 'Friends online: %s' % ', '.join(names)
+    else:
+        return 'No friends online'
+
 
 if __name__ == '__main__':
-
-    j = get_request_api(code)
-    get_names(j)
+    query = create_a_query(argv)
+    if query:
+        request_api = get_request_api(query)
+        get_errors_msg(request_api)
+        friends_online = get_names(request_api)
